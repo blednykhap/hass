c281fb8ef8e70d69ee3e88e610b62739c3032063
andrey.blednykh <blednykhap@gmail.com>
2023-01-17 16:01:38 +0500

    Structure refactoring

---
+++ environment/homeassistant.sh
@@ -0,0 +1,29 @@
+docker pull ghcr.io/home-assistant/home-assistant:stable
+
+docker stop homeassistant
+
+docker rm homeassistant
+
+#install
+sudo docker run -d \
+  --name homeassistant \
+  --privileged \
+  --restart=unless-stopped \
+  -e TZ=Asia/Yekaterinburg \
+  -v /opt/hass/config:/config \
+  --network=host \
+  ghcr.io/home-assistant/home-assistant:stable
+
+#update
+sudo docker run -d \
+  --name homeassistant \
+  --restart=unless-stopped \
+  --privileged \
+  -e TZ=Asia/Yekaterinburg \
+  -v /opt/hass/config:/config \
+  --network=host \
+  ghcr.io/home-assistant/home-assistant:stable
+
+sudo docker exec -it homeassistant bash
+
+sudo docker restart homeassistant
